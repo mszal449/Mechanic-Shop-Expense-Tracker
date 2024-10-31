8df5ce8a8aacaf2d3d089df1c53f6260e67ae1a2
mszal <m.szalasz@gmail.com>
2024-10-31 17:54:03 +0100

    Enhance Navbar styling and update Statistics page layout

--- WebApp/app/statistics/page.tsx
+++ WebApp/app/statistics/page.tsx
@@ -2,7 +2,13 @@ import React from 'react'
 
 const Statistics = () => {
   return (
-    <div>Statistics</div>
+    <>
+      <div className='text-center text-4xl'>Statistics</div>
+      <div className='grid grid-cols-2 px-10 py-4'>
+        <div className='col-span-1 border'>hello</div>
+        <div className='col-span-1 border'>world</div>
+      </div>
+    </>
   )
 }
 
